c73b7525a8a83df0eb1f72ed4ecfc2816dccf2dc
EstebanASM <150808482+EstebanASM@users.noreply.github.com>
2026-01-19 14:30:47 -0500

    feat: :fire: Instalacion de paquetes

--- frontend/my-expo-app/App.tsx
+++ frontend/my-expo-app/App.tsx
@@ -1,23 +1,13 @@
-
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
-
-
-
-  import './global.css';
-
-
-
+import './global.css';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx">
-      
-      </ScreenContent>
+      <ScreenContent title="hola" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
 }
-
